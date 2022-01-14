c2f6782032740342b97eed3ba6ca24800de8f2b7
Piyush <piyush99hjp@gmail.com>
2022-01-14 20:05:04 +0530

    added connection db page

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express=require('express')
+require('./conn')
+const app=express();
+const port=process.env.port||8000;
+app.get("/",(req,res)=>{
+    res.send("welcome to first page");
+})
+app.listen(port,()=>{
+    console.log(`listening to port no ${port}`);
+})
